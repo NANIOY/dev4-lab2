dd5c2d2ae0eb371bdba506ba08d69f01445cd31f
Nanio <111894274+NanioY@users.noreply.github.com>
2024-03-05 09:51:57 +0100

    feat: add welcome screen

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+function isLoggedIn() {
+  return isset($_SESSION['username']);
+}
   function canLogin($username, $password) {
-    if ($username === "Nanio" && $password === "123") {
+    if ($username === "test" && $password === "123") {
       return true;
     } else {
       return false;
@@ -14,11 +17,18 @@
     if (canLogin($username, $password)) {
       session_start();
       $_SESSION['username'] = $username;
-      header("Location: twitch.php");
     } else {
       // ERROR
       $error = true;
     }
+  }
+
+  $page = "";
+
+  switch($page) {
+    case "browser":
+      $page = "Browse some games";
+      break;
   }
 
 ?><!DOCTYPE html>
@@ -38,7 +48,7 @@
           <a href="#" class="loggedIn">
             <div class="user--avatar"><img src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=ddcb7ec744fc63472f2d9e19362aa387" alt=""></div>
            
-            <?php if(isset($_SESSION['username'])): ?>
+            <?php if(isLoggedIn()): ?>
             <h3 class="user--name"><?php echo $_SESSION['username']; ?></h3>
             <?php else: ?>
             <h3 class="user--name">Username here</h3>
@@ -51,38 +61,43 @@
       </header>
       
       <div id="app">
-        <form action="" method="post">
-          <h1>Log in to Twitch</h1>
-          <nav class="nav--login">
-              <a href="#" id="tabLogin">Log in</a>
-              <a href="#" id="tabSignIn">Sign up</a>
-          </nav>
-        
-        <?php if (isset($error)): ?>
-          <div class="alert">That password was incorrect. Please try again</div>
-        <?php endif; ?>
-        
-        <div class="form form--login">
-          <label for="username">Username</label>
-          <input type="text" id="username" name="username">
-        
-          <label for="password">Password</label>
-          <input type="password" id="password" name="password">
-        </div>
-        
-        <div class="form form--signup hidden">
-          <label for="username2">Username</label>
-          <input type="text" id="username2">
-        
-          <label for="password2">Password</label>
-          <input type="password" id="password2">
+        <?php if (!isLoggedIn()): ?>
+          <form action="" method="post">
+            <h1>Log in to Twitch</h1>
+            <nav class="nav--login">
+                <a href="#" id="tabLogin">Log in</a>
+                <a href="#" id="tabSignIn">Sign up</a>
+            </nav>
           
-          <label for="email">Email</label>
-          <input type="text" id="email">
-        </div>
-        
-        <input type="submit" value="Log in" class="btn" id="btnSubmit">
+            <?php if (isset($error)): ?>
+              <div class="alert">That password was incorrect. Please try again</div>
+            <?php endif; ?>
+            
+            <div class="form form--login">
+              <label for="username">Username</label>
+              <input type="text" id="username" name="username">
+            
+              <label for="password">Password</label>
+              <input type="password" id="password" name="password">
+            </div>
+            
+            <div class="form form--signup hidden">
+              <label for="username2">Username</label>
+              <input type="text" id="username2">
+            
+              <label for="password2">Password</label>
+              <input type="password" id="password2">
+              
+              <label for="email">Email</label>
+              <input type="text" id="email">
+            </div>
+            
+            <input type="submit" value="Log in" class="btn" id="btnSubmit">
+          </form>
+          <?php else: ?>
+            <h1>Welcome to twitch <?php echo $_SESSION['username']; ?></h1>
+        <?php endif ?>        
       </div>
-      </form>
+
 </body>
 </html>
